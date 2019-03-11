43c6b6f33081aa9cd8c770479125b848f1ffbfaa
Daniel Schneider <daschne8@gmail.com>
2019-03-11 16:08:33 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,13 @@
+require 'pry'
 
+def sort_array_asc(array)
+  return array.sort
+end
+
+def sort_array_desc(array)
+  return array.sort {|x,y| y<=>x}
+end
+
+def sort_array_char_count(array)
+  return array.sort {|x| x.length}
+end
